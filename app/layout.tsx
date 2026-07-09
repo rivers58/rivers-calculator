271e2200a8287f968fd91d2dad9f97e34c8f58fb
Rivers.Huang <Rivers@ms5.url.com.tw>
2026-07-09 22:09:31 +0800

    Update R-CALC app icon

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata, Viewport } from 'next';
 import './globals.css';
 
 export const metadata: Metadata = {
-  title: 'R-CALC',
+  title: "R-CALC",
   description: 'Rivers Calculator',
   manifest: '/manifest.json',
   icons: {
